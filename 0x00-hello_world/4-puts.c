2cb91ae1f2a8e467de9e52c0cea43c7f393c5051
Hopesharon <hopemsharon@gmail.com>
2023-08-10 09:42:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry
  *
  * Return: always 0
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
-}	
+}
